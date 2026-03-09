f5e2be90ef2f93ed3199c9c3f72375eace084ff4
Thiago Araujo <ta002059@gmail.com>
2026-03-09 18:42:05 -0300

    corrigindo conflito de merge

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://bbsyutjiwfpwkbzccken.supabase.co'
 const supabaseAnonKey = 'sb_publishable_4atDbm3jBhc6ZirCqyBybA_ULUzDFvt'
 
-=======
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://bbsyutjiwfpwkbzccken.supabase.co'
-const supabaseAnonKey = 'sb_publishable_4atDbm3jBhc6ZirCqyBybA_ULUzDFvt'
-
->>>>>>> 022077440ce97099068bb4c89bb44a149e3f0bb4
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
